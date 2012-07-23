5de397b7cdea38cec44cf97c08bdb60962b699c1
Stephen Hopkins <shopkins@usc.edu>
2012-07-23 15:51:33 -0700

    hexagon grid changes -added a summary/usage description at top for how to change the size of the grid -cleaned up the brackets to use less white space -added a few more comments, cleaned up the code a little -the grid can be turned on or off with a checkbox showGrid, on the script in the editor. -fully integrated the script into the prototype scene level
    other changes:
    -moved the terrain ground plane to (0, 0, 0)
    -created a material folder for the hexagon materials

--- Assets/Scripts/Control Scripts/HexagonGrid.js
+++ Assets/Scripts/Control Scripts/HexagonGrid.js
@@ -1,29 +1,40 @@
 #pragma strict
-
-//TODO
-/*
-public struct Tile{
-	TerrainType terrainType;
-	int building; //temp	
-}
-public enum TerrainType{
-	Land, Water	
-}
-public Tile[][] map;
+/**
+Summary of HexagonGrid
+Requirements:
+	-This grid must be attached to a particle system
+		-see the example scene in the test folder for setting up the particle system. Everything must match
+	    -the particle system must have looping turned off. Play on awake turned off, turn everything off except renderer.
+	    In renderer use horizontal billboard and attach the HexParticleMaterial. If the particles start to separate when they are close to the screen, 
+	    increase the max particle size attribute of the renderer
+	-The mainCamera must be linked to the script
+	-The material for the selection hexagon must be linked to the script
+	-The ground plane for the terrain should have y = 0;
+	-The game should not use negative tile coordinates
+Usage:
+	-Hexagon tiles use (x, y) coordinate system. (0, 0, 0) world coordinates, is (0, 0) in tile coordinates,
+    	-uses the positive x/z axis for the tiles
+    -The size/number of hexagon tiles is controlled by the width, and terrainWidth variables.
+    	-the terrain width is the width in world coordinates of the ground plane
+    	-width is the number of hexagonal tiles that fits across this ground plane
+    	-height is the number of hexagon tiles you want vertically
+	-The grid can be turned on or off with the showGrid boolean or the setGridVisibility function
+	-The selection hexagon can be turned on or off with the setSelectionHexagonVisibility function
 */
 
-var mainCamera: Camera;
-var selectionMaterial: Material;
-private var particles: ParticleSystem.Particle[];
+
+var mainCamera: Camera;//set this in the editor, although the script fallbacks to the default camera.
+var selectionMaterial: Material;//material for highlighting a specific hexagon.
+private var hexParticles: ParticleSystem.Particle[];//particles for creating the hex grid
 private var plane:Plane; //plane for raycasting, uses y = 0 as the ground, y-up
 var width: int; //number of tiles horizontally
 var height: int; //number of tiles vertically
 var terrainWidth: float; //width of the hexagonal grid, in world coordinates
 var terrainHeight: float;
+var showGrid:boolean = true;
 var tileWidth: float; //width of a hexagon tile
 var sideSize: float; //size of a single side of the hexagon
-var peakSize: float; //see the ascii art
-var building : Transform; // for placing a building on terrain
+var peakSize: float; //see the ascii art below
 /*<-------> tileWidth
  *   
  *    /\   |peakSize
@@ -35,19 +46,21 @@ var building : Transform; // for placing a building on terrain
  *   \/
  * 
  * */
-	 
+	
+	
+
+ 
 private var hexagon: Mesh; //hexagon mesh for showing selection
 private var selectionHexagon: GameObject;
 
-function Start()
-{
+var building : Transform; // for placing a building on terrain
+function Start(){
 	mainCamera = Camera.main;
 	plane = new Plane();
 	plane.SetNormalAndPosition(new Vector3(0, 1, 0), new Vector3());
-	terrainWidth = 500;
-	terrainHeight = 500;
-	width = 10;
-	height = 8;
+	terrainWidth = 2000;
+	width = 15;
+	height = 10;
 	tileWidth = terrainWidth / width;
 	sideSize = tileWidth / Mathf.Cos(Mathf.PI / 6.0f) / 2.0f;
 	peakSize = sideSize * Mathf.Sin (Mathf.PI / 6.0f);
@@ -65,18 +78,21 @@ function Start()
 	
 }
 
-function Update()
-{
+//converts mouse coordinates to world coordinates to tile coordinates, moves a selection hexagon around the grid.
+function Update(){
+	//shows or hides the grid since this script is attached to a particle system
+	renderer.enabled = showGrid;
+	
 	//get the mouse coordinates, project them onto the plane to get world coordinates of the mouse
 	var mousePosition: Vector3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
 	var ray: Ray = mainCamera.ScreenPointToRay(mousePosition);
 	var enter: float = 0f; //enter stores the distance from the ray to the plane
-	//TODO
 	plane.Raycast(ray, enter);
-	///////
 	var worldPoint: Vector3 = ray.GetPoint(enter);
 	var mouseTile: Vector2 = worldToTileCoordinates(worldPoint.x, worldPoint.z);
-	selectionHexagon.transform.position = new Vector3(mouseTile.x * tileWidth + (mouseTile.y % 2) * tileWidth / 2 , 0.05f, mouseTile.y * sideSize * 1.5f);
+	//0.05f y coordinate the the selection hexagon clips above the terrain//i will clean up the math later
+	var selectionPosition:Vector3 = new Vector3(mouseTile.x * tileWidth + (mouseTile.y % 2) * tileWidth / 2 , 0.05f, mouseTile.y * sideSize * 1.5f);
+	selectionHexagon.transform.position = selectionPosition;
 	
 	//placing a building
 	Debug.Log(	"Position X : " 
@@ -97,37 +113,31 @@ function Update()
 				+ mouseTile.y);
 		
 	var buildPosition: Vector3 = new Vector3(worldPoint.x, -200, worldPoint.z);
-	if ( Input.GetMouseButtonDown(0) )
-   	{
+	if ( Input.GetMouseButtonDown(0) ){
 
       var hit : RaycastHit;      
-      if (Physics.Raycast (ray, hit, 1000.0f))
-      {
+      if (Physics.Raycast (ray, hit, 1000.0f)){
       	 var building = Instantiate(building, buildPosition, Quaternion.identity);
          Debug.Log(hit.collider.gameObject.name);         
       }
-   	}
-	
-	
+   	}		
 }
 
 /*
  * Creates the hexagon that will highlight specific tiles
  * */
-private function createSelectionHexagon()
-{
-	selectionHexagon = new GameObject();
+private function createSelectionHexagon(){
+	selectionHexagon = new GameObject("SelectionHexagon");
 	var meshFilter: MeshFilter = selectionHexagon.AddComponent("MeshFilter");
 	meshFilter.mesh = hexagon;
 	var meshRenderer: MeshRenderer = selectionHexagon.AddComponent("MeshRenderer");
-	if(selectionMaterial == null)
-	{
+	if(selectionMaterial == null){
 		Debug.LogError ("selection material not linked for HexagonGrid");
 	}
 	selectionHexagon.renderer.material = selectionMaterial;
 }
 
-/*hexagon vertex ordering, registration point is lower left corner (not on the hexagon)
+/*hexagon vertex ordering, registration point is the 'o', lower left corner (not on the hexagon)
  * 
  *     0
  *    /\   
@@ -136,17 +146,15 @@ private function createSelectionHexagon()
  * |     |
  *2|     |
  *  \   /4
- *  3\/
+ *o 3\/
  * */
-private function createHexagonMesh()
-{
+private function createHexagonMesh(){
 	hexagon = new Mesh();
 	var vertices: Vector3[];
 	vertices = new Vector3[6];
 	var indices = new Array(0, 5, 1, 1, 5, 2, 5, 4, 2, 2, 4, 3);
 	var radius: float = sideSize;
-	for(var i:int = 0; i < 6; ++i)
-	{
+	for(var i:int = 0; i < 6; ++i){
 		var radian: float = i / 6.0f * Mathf.PI * 2.0f + Mathf.PI / 2.0f; //add PI/2 to start point at top
 		var x: float = tileWidth / 2.0f + Mathf.Cos (radian)* radius;
 		var z: float = sideSize + Mathf.Sin (radian) * radius;
@@ -157,33 +165,34 @@ private function createHexagonMesh()
 	hexagon.triangles = indices;
 	hexagon.RecalculateNormals();
 }
-
-function setGridVisibility(visible:boolean)
-{
-	renderer.enabled = visible;
+//show or hide the grid
+function setGridVisibility(visible:boolean){
+	showGrid = visible;
+	renderer.enabled = showGrid;
 }
 
-function setSelectionHexagonVisbility(visible:boolean)
-{
+//show or hide the hexagon under the mouse
+function setSelectionHexagonVisbility(visible:boolean){
 	selectionHexagon.renderer.enabled = visible;
 }
 
-private function createHexagonGridParticles()
-{
-	particles = new ParticleSystem.Particle[height * width];
-	for(var y:int = 0; y < height; ++y)
-	{
-		for(var x:int = 0; x < width; ++x)
-		{
+
+/*
+creates the hexagonal grid by manually placing sprite particles
+*/
+private function createHexagonGridParticles(){
+	hexParticles = new ParticleSystem.Particle[height * width];
+	for(var y:int = 0; y < height; ++y){
+		for(var x:int = 0; x < width; ++x){
 			var position: Vector3 = new Vector3(x * tileWidth + (y % 2) * tileWidth / 2 + tileWidth / 2 , 0, y * sideSize * 1.5f + (sideSize * 1.5f  + peakSize) / 2);
 			var particle: ParticleSystem.Particle = new ParticleSystem.Particle();
 			particle.position = position;
 			particle.rotation = 45;
 			particle.size = tileWidth * 1.7f;//need to fix this number, I do not know how to get it mathematically
-			particles[y * width + x] = particle;
+			hexParticles[y * width + x] = particle;
 		}
 	}
-	particleSystem.SetParticles(particles, height * width);
+	particleSystem.SetParticles(hexParticles, height * width);
 }
 	
 /*| 0,2 |
@@ -204,8 +213,7 @@ private function createHexagonGridParticles()
  * http://www.gamedev.net/page/resources/_/technical/game-programming/coordinates-in-hexagon-based-tile-maps-r1800
  * converts world coordinates to tile coordinates
  * */
- function worldToTileCoordinates(x:float, y:float): Vector2
- {
+ function worldToTileCoordinates(x:float, y:float): Vector2{
 	var xTile: int;
 	var yTile: int;
 	var xSection: int = Mathf.FloorToInt (x / tileWidth);
@@ -213,56 +221,45 @@ private function createHexagonGridParticles()
 	var xSectionPosition: float = x - xSection * tileWidth;
 	var ySectionPosition: float = y - ySection * (sideSize + peakSize);
 	var sectionType: int;
-	if(ySection % 2 == 0)
-	{
+	if(ySection % 2 == 0){
 		sectionType = 0;
 	}
-	else
-	{
+	else{
 		sectionType = 1;
 	}
 	var slope: float = peakSize / (tileWidth / 2.0f);
-	if(sectionType == 0)
-	{
+	if(sectionType == 0){
 		//default to be in the big area
 		yTile = ySection;
 		xTile = xSection;
-		if(ySectionPosition < peakSize - slope * xSectionPosition) //in lower left triangle
-		{
+		if(ySectionPosition < peakSize - slope * xSectionPosition){ //in lower left triangle
 			xTile = xSection - 1;
 			yTile = ySection - 1;
 		}
-		else if(ySectionPosition < -peakSize + slope * xSectionPosition) //in lower right triangle
-		{
+		else if(ySectionPosition < -peakSize + slope * xSectionPosition){ //in lower right triangle
 			xTile = xSection;
 			yTile = ySection - 1;
 		}
 	}
-	else //section 1
-	{
+	else{ //section 1
 		//right side
-		if(xSectionPosition > tileWidth / 2.0f)
-		{
-			if(ySectionPosition < peakSize * 2 - slope * xSectionPosition) //in bottom middle area
-			{
+		if(xSectionPosition > tileWidth / 2.0f){
+			if(ySectionPosition < peakSize * 2 - slope * xSectionPosition){ //in bottom middle area
 				xTile = xSection;
 				yTile = ySection - 1;
 			}
-			else
-			{
+			else{
 				xTile = xSection;
 				yTile = ySection;
 			}
 		}
 		//on left side
 		else{
-			if(ySectionPosition < slope * xSectionPosition) //in bottom middle area
-			{
+			if(ySectionPosition < slope * xSectionPosition){ //in bottom middle area
 				xTile = xSection;
 				yTile = ySection -1;
 			}
-			else
-			{
+			else{
 				xTile = xSection -1;
 				yTile = ySection;
 			}
