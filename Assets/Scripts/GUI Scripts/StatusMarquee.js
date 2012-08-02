0a9ebb98f47c63989e2b1b663da347cb82209fc8
Ajinkya Waghulde <ajinkya.waghulde@gmail.com>
2012-08-01 17:02:58 -0700

    udpated status marquee

--- Assets/Scripts/GUI Scripts/StatusMarquee.js
+++ Assets/Scripts/GUI Scripts/StatusMarquee.js
@@ -13,12 +13,18 @@ private var mode:String;
 
 function OnGUI()
 {
-	
+	/*
 	if(mode == "explore")
 		GUI.Label(Rect(Screen.width/2 - 50, Screen.height - 40, Screen.width/2 + 50, 40), "<Explore Mode>");	
 	else if (mode == "link")
 		GUI.Label(Rect(Screen.width/2 - 50, Screen.height - 40, Screen.width/2 + 50, 40), "<Link Mode>");	
-
+	*/
+	
+	if(mode == "link")
+		GUI.Label(Rect(Screen.width/2 - 50, Screen.height - 40, Screen.width/2 + 50, 40), "<Link Mode>");	
+	else 
+		GUI.Label(Rect(Screen.width/2 - 50, Screen.height - 40, Screen.width/2 + 50, 40), "<Explore Mode>");	
+	
 
 }
 
