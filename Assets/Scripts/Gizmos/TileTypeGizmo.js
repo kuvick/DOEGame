4af2cb68d0ba9f4d4ca251105a8e3a00edbcd9f4
Stephen Hopkins <shopkins@usc.edu>
2012-08-25 03:15:25 -0700

    Added TileType gizmo (with icons), fixed minor bug/typo in TileEditor, added a comment on how to use the TileEditor
    HexagonGrid.js:
    -made minor changes to for the gizmo to work
    --2 new variables: tileHalfWidth tileHalfHeight
    --added Hill to the enum
    --made tileToWorldCoordinates a static function, works same as before

---
+++ Assets/Scripts/Gizmos/TileTypeGizmo.js
@@ -0,0 +1,43 @@
+#pragma strict
+
+/*
+Author: Stephen Hopkins
+
+Turn on this gizmo to see the type of each tile on the terrain. Uses icons in the Gizmos folder.
+*/
+
+function OnDrawGizmos () {
+    var grid:HexagonGrid = GetComponent("HexagonGrid") as HexagonGrid;
+    if(grid){
+    	for(var y:int = 0; y < grid.height; ++y){
+    		for(var x:int = 0; x < grid.width; ++x){
+    			var iconPos:Vector3 = HexagonGrid.tileToWorldCoordinates(x, y);
+    			iconPos.y = 10;
+    			iconPos.x += grid.tileHalfWidth;//center the icon over the tile
+    			iconPos.z += grid.tileHalfHeight;
+    			switch(grid.getTile(x, y).type){
+    				case TileType.Land:
+    					Gizmos.DrawIcon(iconPos, "land.jpg", true);
+    					break;
+    				case TileType.Hill:
+    					Gizmos.DrawIcon(iconPos, "hill.jpg", true);
+    					break;
+    				case TileType.Water:
+    					Gizmos.DrawIcon(iconPos, "water.jpg", true);
+    				 	break;			
+					case TileType.GeothermalVent:
+						Gizmos.DrawIcon(iconPos, "vent.jpg", true);
+						break;				
+					case TileType.Mine:
+						Gizmos.DrawIcon(iconPos, "mine.jpg", true);
+						break;
+					default:
+						break;
+				}
+					  		
+    		}
+    		
+    	}
+    
+    }
+}
