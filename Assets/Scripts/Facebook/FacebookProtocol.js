7effe36f25a76c77cc189db7127b4df21088b961
jmavis <hellfire073@hotmail.com>
2012-08-27 14:57:35 -0700

    Added in facebook plugin files, setting up the basics for Facebook integration.

---
+++ Assets/Scripts/Facebook/FacebookProtocol.js
@@ -0,0 +1,11 @@
+#pragma strict
+import FacebookInterface;
+
+function Start () {
+	//var myName : String = FacebookInterface.GetUserProperty("Jared.Mavis", "name");
+	//Debug.Log("myName = " + myName);
+}
+
+function Update () {
+
+}
