1b09f65c4906c940242c5b4e2ee1f7964afa5d55
haly <kousetsu.yuan@gmail.com>
2013-01-10 15:27:31 -0500

    - Added "Intelligence" and "Paused" modes

--- Assets/Scripts/GUI Scripts/StatusMarquee.js
+++ Assets/Scripts/GUI Scripts/StatusMarquee.js
@@ -13,39 +13,44 @@ Edits by: Ian Winter, Francis Yuan
 private var mode:int;
 private var welcome : int = 0;
 
-private var marqueePosition:Rect;		// Position of the Status Marquee
 public var marqueeSkin:GUISkin;			// GUISkin component, set in Inspector
+private var marqueePosition:Rect;		// Position of the Status Marquee
+
 private var fontHeightPercent = 0.03;	// Height of the font as a percentage of screen height
 private var fontHeight;					// Height of the font in pixels
 
 function Start()
 {
-	marqueePosition = new Rect(Screen.width/2, 40, 0, 0);
+	// Calclate the marquee position
+	marqueePosition = new Rect(Screen.width/2, Screen.height * 0.05, 0, 0);
 	fontHeight = fontHeightPercent * Screen.height;
+	marqueeSkin.label.fontSize = fontHeight;
+	Debug.Log(fontHeight);
+	Debug.Log(marqueeSkin.label.fontSize);
 }
 
 function OnGUI()
 {
-	/*
-	if(mode == "explore")
-		GUI.Label(Rect(Screen.width/2 - 50, Screen.height - 40, Screen.width/2 + 50, 40), "<Explore Mode>");	
-	else if (mode == "link")
-		GUI.Label(Rect(Screen.width/2 - 50, Screen.height - 40, Screen.width/2 + 50, 40), "<Link Mode>");	
-	*/
-	
-	// Set the font size of the label style of the scoreSkin variable
-	marqueeSkin.label.fontSize = fontHeight;
 	// Set the current GUI's skin to the scoreSkin variable
 	GUI.skin = marqueeSkin;
 	
-	if (mode == GameState.EXPLORE && welcome ==0)
+	if (mode == GameState.EXPLORE && welcome == 0)
 	{
 		GUI.Label(marqueePosition, "Welcome to Prototype City!");
-		//GUI.Label(Rect(Screen.width/2 - 50, Screen.height - 20, Screen.width/2 + 50, 40), "<Explore Mode>");
 	}					
 	else if(mode == GameState.LINK)
 	{
 		GUI.Label(marqueePosition, "<Link Mode>");
+		welcome = 1;
+	}
+	else if(mode == GameState.INTEL)
+	{
+		GUI.Label(marqueePosition, "<Intelligence>");
+		welcome = 1;
+	}
+	else if(mode == GameState.PAUSE)
+	{
+		GUI.Label(marqueePosition, "<Paused>");
 		welcome = 1;
 	}
 	else 
