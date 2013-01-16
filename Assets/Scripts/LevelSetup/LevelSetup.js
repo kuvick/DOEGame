846c7eabfbec801e7a65d913189b619d8f5b85d3
jmavis <jmavis@ucsc.edu>
2013-01-15 21:04:07 -0800

    Added some more aspect ratios.

--- Assets/Scripts/LevelSetup/LevelSetup.js
+++ Assets/Scripts/LevelSetup/LevelSetup.js
@@ -1,15 +1,31 @@
 #pragma strict
 
-enum Ratio {_4x3, _3x2};
+enum Ratio {_4x3, _3x2, _16x9, _5x3, _16x10};
 
 public var wantedScreenRatio : Ratio;
 
 function Awake () {
 	var ratioAmount : float;
-	if (wantedScreenRatio == Ratio._4x3){
-		ratioAmount = 4.0f / 3.0f;
-	} else if (wantedScreenRatio == Ratio._3x2){
-		ratioAmount = 3.0f / 2.0f;
+	
+	switch (wantedScreenRatio){
+		case Ratio._4x3:
+			ratioAmount = 4.0f / 3.0f;
+			break;
+		case Ratio._3x2:
+			ratioAmount = 3.0f / 2.0f;
+			break;
+		case Ratio._16x9:
+			ratioAmount = 16.0f / 9.0f;
+			break;
+		case Ratio._5x3:
+			ratioAmount = 5.0f / 3.0f;
+			break;
+		case Ratio._16x10:
+			ratioAmount = 16.0f / 10.0f;
+			break;
+		default:
+			ratioAmount = 4.0f / 3.0f;	
 	}
+	
 	ScreenSettingsManager.instance.CalculateSettings(ratioAmount);
 }
