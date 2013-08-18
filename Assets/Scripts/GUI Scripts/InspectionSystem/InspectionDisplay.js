9667d36f47d651f617d8921f5d34da40ada75765
d <dejhuey@gmail.com>
2013-08-18 10:49:40 -0700

    Changing tooltip box to static size

--- Assets/Scripts/GUI Scripts/InspectionSystem/InspectionDisplay.js
+++ Assets/Scripts/GUI Scripts/InspectionSystem/InspectionDisplay.js
@@ -11,12 +11,13 @@ private var borderRect : Rect;
 private var borderOffset : float;
 private var borderOffsetScale : float = 0.015;
 private var dispWidth : float;
-private var dispWidthScale : float = .25;
-private var dispHeight : float = 1;
+private var dispWidthScale : float = .8; // width based on box height
+private var dispHeight : float;
+private var dispHeightScale : float = .5;
 private var dispRightOffset : float;
 private var dispRightOffsetScale : float = .025;
 private var dispTopOffset : float;
-private var dispTopOffsetScale : float = .2;
+private var dispTopOffsetScale : float = .25;
 private var fontScale : float = .035;
 private var screenMiddle : Vector2;
 
@@ -31,7 +32,8 @@ private var selectedComponent : InspectionComponent;
 function Start () 
 {
 	screenMiddle = Vector2(Screen.width, Screen.height) / 2.0;
-	dispWidth = Screen.width * dispWidthScale;
+	dispHeight = Screen.height * dispHeightScale;
+	dispWidth = dispHeight * dispWidthScale;
 	dispRightOffset = dispRightOffsetScale * Screen.width;
 	dispTopOffset = dispTopOffsetScale * Screen.height;
 	dispRect = Rect(Screen.width - dispWidth - dispRightOffset,dispTopOffset,dispWidth,dispHeight);
@@ -92,9 +94,9 @@ public function Activate(pic : Texture2D, disp : String, selected : InspectionCo
 // calculates and sets the proper rectangle height and centers it on the screen
 private function FormatDisplay()
 {
-	dispHeight = skin.label.CalcHeight(GUIContent(dispText), dispWidth);
+	/*dispHeight = skin.label.CalcHeight(GUIContent(dispText), dispWidth);
 	dispRect.height = dispHeight;
-	borderRect.height = dispHeight + borderOffset * 2;
+	borderRect.height = dispHeight + borderOffset * 2;*/
 	//dispRect.y = screenMiddle.y - (dispHeight / 2.0);
 }
 
