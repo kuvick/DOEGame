a6f39adfe79c38f68777c5eda0fde453e23170ba
kuvick <katharine.uvick@gmail.com>
2013-11-22 16:52:18 -0500

    (cont syncing)

--- Assets/Scripts/CodexScripts/CodexEntry.js
+++ Assets/Scripts/CodexScripts/CodexEntry.js
@@ -1,7 +1,10 @@
 #pragma strict
 
-public class CodexEntry extends DataEntry {
+public class CodexEntry extends DataEntry
+{
 	public var name : String;
-	public var isUnlocked : boolean;
+	//public var isUnlocked : boolean;
 	public var urlLink: String;
+	public var iconName: String;
+	public var description: String;
 }
