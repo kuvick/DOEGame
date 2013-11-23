fd272486942313a43478915fcbec70d9e4ec36d5
kuvick <katharine.uvick@gmail.com>
2013-11-22 20:17:56 -0500

    CodexEntry.js added
    it wasn't deleted on my local copy, but for some reason github deleted
    it.

--- Assets/Scripts/CodexScripts/CodexEntry.js
+++ Assets/Scripts/CodexScripts/CodexEntry.js
@@ -2,9 +2,11 @@
 
 public class CodexEntry extends DataEntry
 {
+
 	public var name : String;
 	//public var isUnlocked : boolean;
 	public var urlLink: String;
 	public var iconName: String;
 	public var description: String;
+	
 }
