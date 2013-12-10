922d411a6158aefb36318e50b5c62c1432d6d8cb
kuvick <katharine.uvick@gmail.com>
2013-12-10 12:58:10 -0500

    (Because GitHub Deleted a Script)

--- Assets/Scripts/Sound/BackgroundSounds.js
+++ Assets/Scripts/Sound/BackgroundSounds.js
@@ -1,7 +1,8 @@
 #pragma strict
 
 public class BackgroundSounds {
-	public var priority : SoundManager.SoundPriority = SoundManager.SoundPriority.High;
+	//public var priority : SoundManager.SoundPriority = SoundManager.SoundPriority.High;
+	public var priority : SoundManager.SoundPriority = SoundManager.SoundPriority.Low;	// changed so it's on a seperate priority from all the other sounds
 	public var startMenuMusic : SoundType;
 	public var inGameMusic : SoundType;
 	public var pauseMenuMusic : SoundType;
