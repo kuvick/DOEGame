19ea9f57e39e7c51e9377e39497fae1265a98431
d <dejhuey@gmail.com>
2014-05-11 06:51:34 -0700

    Increased size of building menu cancel button

--- Assets/Scripts/GUI Scripts/GUIControls/BuildingMenu.js
+++ Assets/Scripts/GUI Scripts/GUIControls/BuildingMenu.js
@@ -237,11 +237,11 @@ public class BuildingMenu extends GUIControl
 		scrollLeft = createRect(scrollLeftTexture_Active, 0.124, 0.834, 0.079, false);
 		scrollRight = createRect(scrollLeftTexture_Active, 0.65, 0.834, 0.079, false);
 		//cancelButton =	createRect(cancelButtonTexture,0,0, 0.07, false);	
-		cancelButton =	createRect(cancelButtonTexture,0.8,0.6, 0.07, false);	
+		cancelButton =	createRect(cancelButtonTexture,0.8,0.6, 0.095, false);	
 		//cancelButton.x = buildingClip.x;
 		//cancelButton.y = buildingClip.y - cancelButton.height * 1.5;
 		
-		cancelButton.y = cancelButton.height * 0.8;
+		cancelButton.y = cancelButton.height * 0.4;
 		//cancelButton.x = (buildingClip.x + buildingClip.width) - padding;
 
 		
